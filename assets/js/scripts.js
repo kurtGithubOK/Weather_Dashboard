7bb689fa4c7797236f4d52b6f54a55f8061e0d89
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-09-12 23:19:45 -0700

    got elements dyn generating

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,7 @@
 const apiKey = 'f1904d406184f3cd6d2b1fa662fe0acf';
 const openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/';
 
+const forecastDiv = document.getElementById('forecast');
 const searchButton = document.getElementById('searchButton');
 const searchText = document.getElementById('searchText');
 function searchButtonClicked(event) {
@@ -8,8 +9,57 @@ function searchButtonClicked(event) {
 }
 searchButton.addEventListener('click', searchButtonClicked);
 
-const getTodaysWeather = (cityName) => {
+
+//  <div class="col">
+// <div class="card">
+//     <div class="card-body p-2">
+//         <h5 class="card-title">3-31-2021</h5>
+//         <p>icon here</p>
+//         <p class="card-text">Temp: 73.00 def F</p>
+//         <p class="card-text">Wind: 9.53 MPG</p>
+//         <p class="card-text">Humidity: 66 %</p>
+//     </div>
+// </div>
+// </div> 
+const makeElement = (elementName, classArray, textContent) => {
+    const element = document.createElement(elementName);
+    for(classArrayElement of classArray) {
+        console.log(classArrayElement)
+        element.classList.add(classArrayElement);
+    }
+    element.textContent = textContent;
+    return element;
 };
+
+// Create Card from the inside out.
+const makeForecastCards = (forecastData) => {
+    // for (let i = 0; i < forecastData.length; i++) { /////////////// change this at end
+    for (let i = 0; i < 5; i++) {
+        const forecastDay = forecastData[i];
+        const titleElement = makeElement('h5', ['card-title'], '6-6-66');
+        const iconElement = makeElement('p', ['card-text'], 'icon here??');
+        const tempElement = makeElement('p', ['card-text'], 'Temp: ' + 666);
+        const windElement = makeElement('p', ['card-text'], 'Wind: ' + 66);
+        const humidityElement = makeElement('p', ['card-text'], 'Humidity: ' + 99);
+        
+        const cardBodyElement = makeElement('div', ['card-body', 'p-2']);
+        cardBodyElement.appendChild(titleElement);
+        cardBodyElement.appendChild(iconElement);
+        cardBodyElement.appendChild(tempElement);
+        cardBodyElement.appendChild(windElement);
+        cardBodyElement.appendChild(humidityElement);
+
+        const cardElement = makeElement('div', ['card']);
+        cardElement.appendChild(cardBodyElement);
+
+        const colElement = makeElement('div', ['col']);
+        colElement.appendChild(cardElement);
+        forecastDiv.appendChild(colElement);
+    }
+};
+makeForecastCards([]);
+
+
 const makeForecastWeatherUrl = (cityName) => {
     // https://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=f1904d406184f3cd6d2b1fa662fe0acf&units=imperial
     let url = openWeatherMapUrl;
@@ -53,32 +103,37 @@ const todaysWeatherUrl = makeTodaysWeatherUrl(cityName);
 
 const forecastWeatherUrl = makeForecastWeatherUrl(cityName);
 
-fetch(forecastWeatherUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then((data) => {
-        const forecastList = data.list;
-        for (let i = 0; i < 5; i++) {
-            const forecastListItem = forecastList[i];
-            console.log('forecastListItem', forecastListItem);
+// fetch(forecastWeatherUrl)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then((data) => {
+//         const forecastList = data.list;
+//         const forecastArray = [];
+//         for (let i = 0; i < 5; i++) {
+//             const forecastListItem = forecastList[i];
+//             const forecastArrayItem = [];
+//             console.log('forecastListItem', forecastListItem);
 
-            // const date = forecastListItem.dt;
-            // console.log('date', date);
-            // const moment = moment(date, 'X');
-            // console.log('xxxxxx: ', moment.toString());
+//             // const date = forecastListItem.dt;
+//             // console.log('date', date);
+//             // const moment = moment(date, 'X');
+//             // console.log('xxxxxx: ', moment.toString());
 
-            const temperature = forecastListItem.main.temp;
-            const windSpeed = forecastListItem.wind.speed;
-            const humidity = forecastListItem.main.humidity;
-            console.log('humidity', humidity)
-
-        }
-
-    })
-    .catch(function (err) {
-        console.log("Something went wrong!", err);
-    });
+//             const temperature = forecastListItem.main.temp;
+//             const windSpeed = forecastListItem.wind.speed;
+//             const humidity = forecastListItem.main.humidity;
+//             forecastListItem.temperature = temperature;
+//             forecastListItem.windSpeed = windSpeed;
+//             forecastListItem.humidity = humidity;
+//             forecastArray.push(forecastListItem);
+//             console.log('humidity', humidity)
+//         }
+//         return forecastArray;
+//     })
+//     .catch(function (err) {
+//         console.log("Something went wrong!", err);
+//     });
 
 
 
