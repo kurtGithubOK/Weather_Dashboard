551f500c87b4dc264b5bc79873d04a2acf54f729
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-09-12 17:56:42 -0700

    Testing vscode commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@ const searchButton = document.getElementById('searchButton');
 const searchText = document.getElementById('searchText');
 
 function searchButtonClicked(event) {
-    console.log('clcikee', searchText);
+    console.log('clcikcee', searchText);
     
 }
 
