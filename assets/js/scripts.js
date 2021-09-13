9f027367017b7681e830ba8cdf2bba8c90314e18
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-09-13 13:41:45 -0700

    Re-do JS logic, get todays weather to display.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,40 +9,51 @@ const searchButton = document.getElementById('searchButton');
 const searchText = document.getElementById('searchText');
 const cityListDiv = document.getElementById('cityList');
 
+const todaysTempDiv = document.getElementById('todaysTemp');
+const todaysWindDiv = document.getElementById('todaysWind');
+const todaysHumidityDiv = document.getElementById('todaysHumidity');
+const todaysUVIndex = document.getElementById('todaysUVIndex');
+
 // Register event handlers.
 searchButton.addEventListener('click', searchButtonClicked);
 
 function updateDisplay() {
-// Update list of cities.
+    // Update list of cities.
     updateListOfCities();
 
     // Update today's weather.
     updateTodaysWeather();
-    
+
     // Update forecast.
     updateForecast();
 }
 
+// Loop over list of cities and make buttons for each.
 function updateListOfCities() {
-    // Loop over list of cities and make buttons for each.
-    previousCities.forEach( (city) => {
-        // <button type="button" class="btn btn-secondary w-100 my-1">Austin</button>
+    previousCities.forEach((city) => {
         const buttonElement = document.createElement('button');
         buttonElement.classList.add('btn', 'btn-secondary', 'w-100', 'my-1');  // Change other code like this.
         buttonElement.textContent = city;
         cityListDiv.appendChild(buttonElement);
     });
 }
+
 function updateTodaysWeather() {
+    const todaysWeatherUrl = makeTodaysWeatherUrl(previousCities[0]);
     // Make API call.
-    
-    // Update upper section.
-    
+    doFetch(todaysWeatherUrl)
+        .then((data) => {
+            populateTodaysWeather(data);
+        });
 }
+
 function updateForecast() {
+    const forecastUrl = makeForecastWeatherUrl(previousCities[0]);
     // Make API call.
-    
-    // Update lower section.
+    doFetch(forecastUrl)
+        .then(data => {
+            populateForecast(data);
+        });
 }
 
 // Event handlers. 
@@ -59,6 +70,89 @@ function cityClicked(event) {
 
 }
 
+// Utility functions ////////////////////////////////////////////////////////////
+const populateTodaysWeather = data => {
+    todaysTempDiv.textContent = data.main.temp;
+    todaysWindDiv.textContent = data.wind.speed;
+    todaysHumidityDiv.textContent = data.main.humidity;
+    // HOW TO GET UV INDEX???????????????????
+};
+
+const populateForecast = data => {
+
+
+    //         const forecastList = data.list;
+    //         const forecastArray = [];
+    //         for (let i = 0; i < 5; i++) {
+    //             const forecastListItem = forecastList[i];
+    //             const forecastArrayItem = [];
+    //             console.log('forecastListItem', forecastListItem);
+
+    //             // const date = forecastListItem.dt;
+    //             // console.log('date', date);
+    //             // const moment = moment(date, 'X');
+    //             // console.log('xxxxxx: ', moment.toString());
+
+    //             const temperature = forecastListItem.main.temp;
+    //             const windSpeed = forecastListItem.wind.speed;
+    //             const humidity = forecastListItem.main.humidity;
+    //             forecastListItem.temperature = temperature;
+    //             forecastListItem.windSpeed = windSpeed;
+    //             forecastListItem.humidity = humidity;
+    //             forecastArray.push(forecastListItem);
+    //             console.log('humidity', humidity)
+    //         }
+    //         return forecastArray;
+
+};
+
+const doFetch = (url) => {
+    return fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .catch(function (err) {
+            console.log("Something went wrong calling this url:", url, err);
+        });
+};
+
+const makeElement = (elementName, classArray, textContent) => {
+    const element = document.createElement(elementName);
+    for (classArrayElement of classArray) {
+        console.log(classArrayElement)
+        element.classList.add(classArrayElement);
+    }
+    element.textContent = textContent;
+    return element;
+};
+
+// Create Card from the inside out.
+const makeForecastCards = (forecastData) => {
+    // for (let i = 0; i < forecastData.length; i++) { /////////////// change this at end
+    for (let i = 0; i < 5; i++) {
+        const forecastDay = forecastData[i];
+        const titleElement = makeElement('h5', ['card-title'], '6-6-66');
+        const iconElement = makeElement('p', ['card-text'], 'icon here??');
+        const tempElement = makeElement('p', ['card-text'], 'Temp: ' + 666);
+        const windElement = makeElement('p', ['card-text'], 'Wind: ' + 66);
+        const humidityElement = makeElement('p', ['card-text'], 'Humidity: ' + 99);
+
+        const cardBodyElement = makeElement('div', ['card-body', 'p-2']);
+        cardBodyElement.appendChild(titleElement);
+        cardBodyElement.appendChild(iconElement);
+        cardBodyElement.appendChild(tempElement);
+        cardBodyElement.appendChild(windElement);
+        cardBodyElement.appendChild(humidityElement);
+
+        const cardElement = makeElement('div', ['card']);
+        cardElement.appendChild(cardBodyElement);
+
+        const colElement = makeElement('div', ['col']);
+        colElement.appendChild(cardElement);
+        forecastDiv.appendChild(colElement);
+    }
+};
+// makeForecastCards([]);
 
 const makeForecastWeatherUrl = (cityName) => {
     // https://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=f1904d406184f3cd6d2b1fa662fe0acf&units=imperial
@@ -79,100 +173,6 @@ function makeTodaysWeatherUrl(cityName) {
     url += '&units=imperial';
     return url;
 }
-
-const cityName = 'seattle';
-const todaysWeatherUrl = makeTodaysWeatherUrl(cityName);
-
-// fetch(todaysWeatherUrl)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then((data) => {
-//         console.log('weather data: ', data);
-//         const temerature = data.main.temp;
-//         console.log('Temp:', temerature);
-//         const windSpeed = data.wind.speed;
-//         console.log('windSpeed:', windSpeed)
-//         const humidity = data.main.humidity;
-//         console.log('Humidity:', humidity)
-//         // HOW TO GET UV INDEX???????????????????
-//     })
-//     .catch(function (err) {
-//         console.log("Something went wrong!", err);
-//     });
-
-const forecastWeatherUrl = makeForecastWeatherUrl(cityName);
-
-// fetch(forecastWeatherUrl)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then((data) => {
-//         const forecastList = data.list;
-//         const forecastArray = [];
-//         for (let i = 0; i < 5; i++) {
-//             const forecastListItem = forecastList[i];
-//             const forecastArrayItem = [];
-//             console.log('forecastListItem', forecastListItem);
-
-//             // const date = forecastListItem.dt;
-//             // console.log('date', date);
-//             // const moment = moment(date, 'X');
-//             // console.log('xxxxxx: ', moment.toString());
-
-//             const temperature = forecastListItem.main.temp;
-//             const windSpeed = forecastListItem.wind.speed;
-//             const humidity = forecastListItem.main.humidity;
-//             forecastListItem.temperature = temperature;
-//             forecastListItem.windSpeed = windSpeed;
-//             forecastListItem.humidity = humidity;
-//             forecastArray.push(forecastListItem);
-//             console.log('humidity', humidity)
-//         }
-//         return forecastArray;
-//     })
-//     .catch(function (err) {
-//         console.log("Something went wrong!", err);
-//     });
-
-// Utility functions.
-const makeElement = (elementName, classArray, textContent) => {
-    const element = document.createElement(elementName);
-    for(classArrayElement of classArray) {
-        console.log(classArrayElement)
-        element.classList.add(classArrayElement);
-    }
-    element.textContent = textContent;
-    return element;
-};
-
-// Create Card from the inside out.
-const makeForecastCards = (forecastData) => {
-    // for (let i = 0; i < forecastData.length; i++) { /////////////// change this at end
-    for (let i = 0; i < 5; i++) {
-        const forecastDay = forecastData[i];
-        const titleElement = makeElement('h5', ['card-title'], '6-6-66');
-        const iconElement = makeElement('p', ['card-text'], 'icon here??');
-        const tempElement = makeElement('p', ['card-text'], 'Temp: ' + 666);
-        const windElement = makeElement('p', ['card-text'], 'Wind: ' + 66);
-        const humidityElement = makeElement('p', ['card-text'], 'Humidity: ' + 99);
-        
-        const cardBodyElement = makeElement('div', ['card-body', 'p-2']);
-        cardBodyElement.appendChild(titleElement);
-        cardBodyElement.appendChild(iconElement);
-        cardBodyElement.appendChild(tempElement);
-        cardBodyElement.appendChild(windElement);
-        cardBodyElement.appendChild(humidityElement);
-
-        const cardElement = makeElement('div', ['card']);
-        cardElement.appendChild(cardBodyElement);
-
-        const colElement = makeElement('div', ['col']);
-        colElement.appendChild(cardElement);
-        forecastDiv.appendChild(colElement);
-    }
-};
-// makeForecastCards([]);
 
 // Initialize & start app.
 function init() {
