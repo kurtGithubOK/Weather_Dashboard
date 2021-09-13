ec45da6601e6e9bf02f1301346d7b35c33e5fec0
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-09-12 18:51:10 -0700

    Got today's weather api working!

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,79 +1,48 @@
-
+const apiKey = 'f1904d406184f3cd6d2b1fa662fe0acf';
+const openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/';
 
 const searchButton = document.getElementById('searchButton');
 const searchText = document.getElementById('searchText');
-
 function searchButtonClicked(event) {
-    console.log('clcikcffffffeddde', searchText);
-    
+    console.log('now in searchButtonClicked():', searchText);
 }
-
 searchButton.addEventListener('click', searchButtonClicked);
 
-const cityName = 'seattle';
-const apiKey = 'f1904d406184f3cd6d2b1fa662fe0acf';
-
-function generateURL(cityName, apiKey) {
-    let url = 'https://api.openweathermap.org/data/2.5/weather';
-    url += '?q=';
-    url += cityName;
-    url += '&appId=';
-    url += apiKey;
-    return url;    
+const getTodaysWeather = (cityName) => {
+};    
+const getForecast = (cityName) => {
+    // https://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=f1904d406184f3cd6d2b1fa662fe0acf&units=imperial
 }
 
-const url = generateURL(cityName, apiKey);
-console.log('weather url', url)
+function makeTodaysWeatherUrl(cityName) {
+    // https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=f1904d406184f3cd6d2b1fa662fe0acf&units=imperial
+    let url = openWeatherMapUrl;
+    url += 'weather';
+    url += '?q=' + cityName;
+    url += '&appId=' + apiKey;
+    url += '&units=imperial';
+    return url;
+}
 
+const cityName = 'seattle';
+const todaysWeatherUrl = makeTodaysWeatherUrl(cityName, apiKey);
+console.log('todays weather url', todaysWeatherUrl)
 
-fetch(url)
-.then(function (response) {
-    console.log('weather response: ', response.json());
-})
-.then( (data) => {
-    console.log('weather data: ', data);
+fetch(todaysWeatherUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then((data) => {
+        console.log('weather data: ', data);
+        const temerature = data.main.temp;
+        console.log('Temp:', temerature);
+        const windSpeed = data.wind.speed;
+        console.log('windSpeed:', windSpeed)
+        const humidity = data.main.humidity;
+        console.log('Humidity:', humidity)
+        // HOW TO GET UV INDEX???????????????????
+    })
+    .catch(function (err) {
+        console.log("Something went wrong!", err);
+    });
 
-})
-.catch(function (err) {
-    console.log("Something went wrong!", err);
-});
-
-console.log('')
-
-/////////////////////////////////////
-const seattleLongitude = -122.3321;
-const seattleLatitude = 47.6062;
-const apiQuery = 'forecast?';
-let forecastURL = 'https://api.openweathermap.org/data/2.5/';
-forecastURL += apiQuery;
-forecastURL += 'q=' + cityName;
-forecastURL += '&appid=' + apiKey;
-
-console.log('forecastURL', forecastURL)
-
-
-// fetch('https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&appid=' + apiKey)
-fetch(forecastURL)
-.then(function (response) {
-    console.log('Forecast: ', response);
-})
-.catch(function (err) {
-    console.log("Something went wrong!", err);
-});
-
-
-// "coord": {
-//     "lon": ,
-//     "lat": 
-//   },
-
-// ?lat={lat}
-// &lon={lon}
-// &exclude={part}
-// &appid={API key}
-
-/*
-https://openweathermap.org/api/one-call-api#data
-https://openweathermap.org/api/one-call-api#parameter
-
-*/
