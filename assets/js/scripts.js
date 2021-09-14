ed7af4631c9b16b5d4b79fdfe543c8348e4bcc7e
Kurt Heimerman <kurtheimerman@yahoo.com>
2021-09-13 18:03:43 -0700

    clean up

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,12 @@
+
+// Global variables.
 const apiKey = 'f1904d406184f3cd6d2b1fa662fe0acf';
 const openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/';
 let previousCities = ['seattle', 'anchorage', 'denver']; // Trim down to one later.
 let previousCityIndex = 0;
 
+
 // Variables for divs.
-const forecastDiv = document.getElementById('forecast');
 const searchButton = document.getElementById('searchButton');
 const searchText = document.getElementById('searchText');
 const cityListDiv = document.getElementById('cityList');
@@ -14,17 +16,19 @@ const todaysWindDiv = document.getElementById('todaysWind');
 const todaysHumidityDiv = document.getElementById('todaysHumidity');
 const todaysUVIndex = document.getElementById('todaysUVIndex');
 
+const forecastDiv = document.getElementById('forecast');
+
 // Register event handlers.
 searchButton.addEventListener('click', searchButtonClicked);
 
+
+
+// Functions to update values displayed on page.
 function updateDisplay() {
-    // Update list of cities.
+    // City search
     updateListOfCities();
-
-    // Update today's weather.
     updateTodaysWeather();
-
-    // Update forecast.
+    // Forecast cards
     updateForecast();
 }
 
@@ -38,6 +42,7 @@ function updateListOfCities() {
     });
 }
 
+// Make API call and display response in today's weather.
 function updateTodaysWeather() {
     const todaysWeatherUrl = makeTodaysWeatherUrl(previousCities[0]);
     // Make API call.
@@ -47,16 +52,21 @@ function updateTodaysWeather() {
         });
 }
 
+// Make API call and display response in forecast.
 function updateForecast() {
     const forecastUrl = makeForecastWeatherUrl(previousCities[0]);
     // Make API call.
     doFetch(forecastUrl)
         .then(data => {
-            populateForecast(data);
+            // Transform incoming data into data objects.
+            const extractedForecastData = extractForecastData(data);
+            // Generate Cards for each forecast day.
+            makeForecastCards(extractedForecastData);
         });
 }
 
-// Event handlers. 
+
+// Event handlers. ////////////////////////////////////////////////////////////
 function searchButtonClicked(event) {
     console.log('now in searchButtonClicked():', searchText);
     // Store city name in variable.
@@ -70,7 +80,9 @@ function cityClicked(event) {
 
 }
 
+
 // Utility functions ////////////////////////////////////////////////////////////
+// Update display.
 const populateTodaysWeather = data => {
     todaysTempDiv.textContent = data.main.temp;
     todaysWindDiv.textContent = data.wind.speed;
@@ -78,7 +90,34 @@ const populateTodaysWeather = data => {
     // HOW TO GET UV INDEX???????????????????
 };
 
-const populateForecast = data => {
+// Loop over forecast data & assemble Card divs from the inside out.
+const makeForecastCards = (forecastData) => {
+    for (let i = 0; i < forecastData.length; i++) {
+        const forecastDay = forecastData[i];
+        const titleElement = makeElement('h5', ['card-title'], forecastDay.forecastDate);
+        const iconElement = makeElement('p', ['card-text'], 'icon here??');
+        const tempElement = makeElement('p', ['card-text'], 'Temp: ' + forecastDay.temperature);
+        const windElement = makeElement('p', ['card-text'], 'Wind: ' + forecastDay.windSpeed);
+        const humidityElement = makeElement('p', ['card-text'], 'Humidity: ' + forecastDay.humidity);
+
+        const cardBodyElement = makeElement('div', ['card-body', 'p-2']);
+        cardBodyElement.appendChild(titleElement);
+        cardBodyElement.appendChild(iconElement);
+        cardBodyElement.appendChild(tempElement);
+        cardBodyElement.appendChild(windElement);
+        cardBodyElement.appendChild(humidityElement);
+
+        const cardElement = makeElement('div', ['card']);
+        cardElement.appendChild(cardBodyElement);
+
+        const colElement = makeElement('div', ['col']);
+        colElement.appendChild(cardElement);
+        forecastDiv.appendChild(colElement);
+    }
+};
+
+// Returns an array of objects containing data for each forecast day.
+const extractForecastData = data => {
     const forecastDataList = data.list;
     // Put data into array of objects for later use.
     const forecastDataArray = [];
@@ -97,6 +136,16 @@ const populateForecast = data => {
     return forecastDataArray;
 };
 
+// Utility to make a generic HTML element.
+const makeElement = (elementName, classArray, textContent) => {
+    const element = document.createElement(elementName);
+    for (classArrayElement of classArray) {                // s/b shorter version???????????????
+        element.classList.add(classArrayElement);
+    }
+    element.textContent = textContent;
+    return element;
+};
+
 const doFetch = (url) => {
     return fetch(url)
         .then(response => {
@@ -106,44 +155,6 @@ const doFetch = (url) => {
             console.log("Something went wrong calling this url:", url, err);
         });
 };
-
-const makeElement = (elementName, classArray, textContent) => {
-    const element = document.createElement(elementName);
-    for (classArrayElement of classArray) {
-        console.log(classArrayElement)
-        element.classList.add(classArrayElement);
-    }
-    element.textContent = textContent;
-    return element;
-};
-
-// Create Card from the inside out.
-const makeForecastCards = (forecastData) => {
-    // for (let i = 0; i < forecastData.length; i++) { /////////////// change this at end
-    for (let i = 0; i < 5; i++) {
-        const forecastDay = forecastData[i];
-        const titleElement = makeElement('h5', ['card-title'], '6-6-66');
-        const iconElement = makeElement('p', ['card-text'], 'icon here??');
-        const tempElement = makeElement('p', ['card-text'], 'Temp: ' + 666);
-        const windElement = makeElement('p', ['card-text'], 'Wind: ' + 66);
-        const humidityElement = makeElement('p', ['card-text'], 'Humidity: ' + 99);
-
-        const cardBodyElement = makeElement('div', ['card-body', 'p-2']);
-        cardBodyElement.appendChild(titleElement);
-        cardBodyElement.appendChild(iconElement);
-        cardBodyElement.appendChild(tempElement);
-        cardBodyElement.appendChild(windElement);
-        cardBodyElement.appendChild(humidityElement);
-
-        const cardElement = makeElement('div', ['card']);
-        cardElement.appendChild(cardBodyElement);
-
-        const colElement = makeElement('div', ['col']);
-        colElement.appendChild(cardElement);
-        forecastDiv.appendChild(colElement);
-    }
-};
-// makeForecastCards([]);
 
 const makeForecastWeatherUrl = (cityName) => {
     // https://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=f1904d406184f3cd6d2b1fa662fe0acf&units=imperial
